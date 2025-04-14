9675019760b06b5361b176b21f44172ebc2014d8
Sid Dixit <dixit.siddhant22@gmail.com>
2025-04-14 02:13:05 -0400

    Finished Problem #3

---
+++ #3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        total = 0
+        left = 0
+        chars = set()
+        
+        for right in range(len(s)):
+            if s[right] not in chars:
+                chars.add(s[right])
+                total = max(total, right - left + 1)
+            else:
+                while s[right] in chars:
+                    chars.remove(s[left])
+                    left += 1
+                chars.add(s[right])
+
+        return total
+    
+## Took time because I forgot about sets
