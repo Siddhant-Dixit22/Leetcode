a3eff99922dc2f3c6f30f6f0b9a8a1f59ea8bccd
Sid Dixit <dixit.siddhant22@gmail.com>
2025-04-14 00:33:29 -0400

    Brute Force way of finishing Two Sum

---
+++ #1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        sol = [] * 2
+
+        for i in range(len(nums)):
+            curr = nums[i]
+            for j in range(i + 1, len(nums)):
+                if (target == (nums[j] + curr)):
+                    sol.append(i)
+                    sol.append(j)
+                    return sol
